f84e493bdad86b5b8412e065ae0e9d934d640bf7
OtnielR <endidebora@gmail.com>
2025-09-28 16:33:32 +0700

    first commit

---
+++ components/main-header/main-header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import classes from "./main-header.module.css"
+import logoImg from "@/assets/logo.png"
+
+import NavLink from "./nav-link"
+import MainHeaderBackground from "./main-header-background"
+
+export default function MainHeader() {
+    return <>
+        <MainHeaderBackground></MainHeaderBackground>
+        <header className={classes.header}>
+
+            <Link href='/' className={classes.logo}>
+                <Image src={logoImg} alt="A Plate with food on it" priority />
+                NextLevel Food
+            </Link>
+
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <NavLink href='/meals'>Browse Meals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href='/community'>Foodies Community</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+}
